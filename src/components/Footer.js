a0e1bdb63a7757c17555ab055cb1f35d55dc4566
NevenPolsak <nevenpolsak1@gmail.com>
2022-03-14 16:06:09 +0100

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{backgroundColor:"blue"}}>
+        <p style={{color:"white"}}>© 2002</p>
+        
+    </div>
+  )
+}
+
+export default Footer
